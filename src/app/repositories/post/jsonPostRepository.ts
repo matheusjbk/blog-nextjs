d28d987a33c766f40b1f5acaa7a8ff6a88d0d43c
matheusjbk <matheusjbk@gmail.com>
2026-02-11 12:10:30 -0300

    feat: adding post repository

---
+++ src/app/repositories/post/jsonPostRepository.ts
@@ -0,0 +1,38 @@
+import { PostModel } from "@/models/post/postModel";
+import { PostRepository } from "./postRepository";
+import { resolve } from "path";
+import { readFile } from "fs/promises";
+
+const ROOT_DIR = process.cwd();
+const JSON_POSTS_FILE_PATH = resolve(
+  ROOT_DIR,
+  "src",
+  "db",
+  "seed",
+  "posts.json",
+);
+
+export class JsonPostRepository implements PostRepository {
+  async readFromDisk(): Promise<PostModel[]> {
+    const jsonContent = await readFile(JSON_POSTS_FILE_PATH, "utf-8");
+    const parsedJson = JSON.parse(jsonContent);
+    const { posts } = parsedJson;
+    return posts;
+  }
+
+  async findAll(): Promise<PostModel[]> {
+    const posts = await this.readFromDisk();
+    return posts;
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const posts = await this.findAll();
+    const post = posts.find(post => post.id === id);
+
+    if (!post) throw new Error("Post não encontrado");
+
+    return post;
+  }
+}
+
+export const jsonPostRepository: PostRepository = new JsonPostRepository();
