8f253dcc5419c1e47fcb2b2ff24b22ce76332f1f
matheusjbk <matheusjbk@gmail.com>
2026-02-12 14:58:04 -0300

    feat: grid in featured post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,47 @@
 import { Container } from "@/components/Container";
+import { Header } from "@/components/Header";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
+import Image from "next/image";
+import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function Home() {
   return (
     <Container>
-      <header className="text-6xl font-bold py-4 text-center">HEADER</header>
+      <Header />
+      <section className="mb-8 group grid grid-cols-1 gap-8 sm:grid-cols-2">
+        <Link
+          href="#"
+          className="w-full h-full overflow-hidden rounded-xl"
+        >
+          <Image
+            className="w-full h-full object-cover object-center group-hover:scale-105 transition"
+            src="/images/bryen_0.png"
+            width={1200}
+            height={720}
+            alt="Título do post"
+          />
+        </Link>
+
+        <div className="flex flex-col gap-4 sm:justify-center">
+          <time
+            dateTime="2026-02-12"
+            className="text-slate-600 text-sm/tight"
+          >
+            12/02/2026 12:00
+          </time>
+          <h2 className="text-2xl/tight font-extrabold sm:text-4xl">
+            Título do post
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            eveniet, deserunt beatae inventore dignissimos reiciendis. Facere
+            sint ipsum minima labore, velit tempora vel laboriosam. Maiores,
+            nam! Illum ex voluptatem cupiditate!
+          </p>
+        </div>
+      </section>
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header>
+      <h1 className="text-4xl font-extrabold py-8 md:text-6xl md:py-10 lg:text-7xl lg:py-12">
+        <Link href="#">The Blog</Link>
+      </h1>
+    </header>
+  );
+}
