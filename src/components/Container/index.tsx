c1bbc758d4b0243e80f78eed88ec955ff724f06a
matheusjbk <matheusjbk@gmail.com>
2026-02-12 14:07:24 -0300

    chore: changing class name

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -5,7 +5,7 @@ type ContainerProps = {
 export function Container({ children }: ContainerProps) {
   return (
     <div className="bg-slate-100 text-slate-900 min-h-screen dark:bg-stone-950 dark:text-slate-100">
-      <div className="max-w-screen-lg mx-auto px-8">{children}</div>
+      <div className="max-w-5xl mx-auto px-8">{children}</div>
     </div>
   );
 }
