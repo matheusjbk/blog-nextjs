6656c22a8211cc7cef8ecfe3b2886039fee73450
matheusjbk <matheusjbk@gmail.com>
2026-02-13 12:41:46 -0300

    chore: changing layout of root page and title metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Container } from "@/components/Container";
+import { Header } from "@/components/Header";
 
 export const metadata: Metadata = {
-  title: "The Blog - Um blog criado com Next.js",
+  title: {
+    default: "The Blog - Um blog criado com Next.js",
+    template: "%s | The Blog",
+  },
   description: "Descrição da página",
 };
 
@@ -16,7 +21,17 @@ export default function RootLayout({
       lang="pt-BR"
       className=""
     >
-      <body>{children}</body>
+      <body>
+        <Container>
+          <Header />
+
+          {children}
+
+          <footer className="text-6xl font-bold py-4 text-center">
+            FOOTER
+          </footer>
+        </Container>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import { Container } from "@/components/Container";
 import { FeaturedPost } from "@/components/FeaturedPost";
-import { Header } from "@/components/Header";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
 
 export default async function Home() {
   return (
-    <Container>
-      <Header />
-
+    <>
       <Suspense fallback={<SpinLoader />}>
         <FeaturedPost />
       </Suspense>
@@ -17,7 +13,6 @@ export default async function Home() {
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
-      <footer className="text-6xl font-bold py-4 text-center">FOOTER</footer>
-    </Container>
+    </>
   );
 }
