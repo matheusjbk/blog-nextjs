d8b1b473761784577250b8d0cb02d6e18912b449
matheusjbk <matheusjbk@gmail.com>
2026-03-14 11:39:41 -0300

    feat: adding text input component

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,60 +1,44 @@
-import { Button } from "@/components/Button";
-import { BugIcon } from "lucide-react";
+import { InputText } from "@/components/InputText";
 
 export default async function AdminNewPostPage() {
   return (
     <>
-      <div className="flex gap-2 py-1 items-center">
-        <Button
-          color="default"
-          size="sm"
-        >
-          <BugIcon />
-          Default
-        </Button>
+      <div className="">
+        <InputText
+          labelText="Nome"
+          placeholder="Digite o seu nome"
+        />
 
-        <Button
-          color="ghost"
-          size="md"
-        >
-          <BugIcon />
-          Ghost
-        </Button>
+        <InputText
+          labelText="Sobrenome"
+          placeholder="Digite o seu sobrenome"
+        />
 
-        <Button
-          color="danger"
-          size="lg"
-        >
-          <BugIcon />
-          Danger
-        </Button>
-      </div>
+        <InputText
+          disabled
+          labelText="Nome"
+          placeholder="Digite o seu nome"
+        />
 
-      <div className="flex gap-2 py-1 items-center">
-        <Button
-          color="default"
-          size="sm"
+        <InputText
           disabled
-        >
-          <BugIcon />
-          Default
-        </Button>
-        <Button
-          color="ghost"
-          size="md"
-          disabled
-        >
-          <BugIcon />
-          Ghost
-        </Button>
-        <Button
-          color="danger"
-          size="lg"
-          disabled
-        >
-          <BugIcon />
-          Danger
-        </Button>
+          labelText="Sobrenome"
+          placeholder="Digite o seu sobrenome"
+          defaultValue="Silva"
+        />
+
+        <InputText
+          readOnly
+          labelText="Nome"
+          placeholder="Digite o seu nome"
+        />
+
+        <InputText
+          readOnly
+          labelText="Sobrenome"
+          placeholder="Digite o seu sobrenome"
+          defaultValue="Silva"
+        />
       </div>
     </>
   );
---
+++ src/components/InputText/index.tsx
@@ -0,0 +1,20 @@
+import { useId } from "react";
+
+type InputTextProps = {
+  labelText?: string;
+} & React.ComponentProps<"input">;
+
+export function InputText({ labelText = "", ...props }: InputTextProps) {
+  const id = useId();
+
+  return (
+    <div className="flex flex-col gap-1">
+      {labelText && <label htmlFor={id}>{labelText}</label>}
+      <input
+        id={id}
+        {...props}
+        className="bg-white outline-0 text-base/tight ring-2 ring-slate-500 focus:ring-blue-600 rounded p-2 transition placeholder-slate-400 disabled:bg-slate-300 disabled:placeholder-slate-500 disabled:text-slate-500 read-only:bg-slate-200"
+      />
+    </div>
+  );
+}
