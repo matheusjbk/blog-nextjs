229fc17398efb49038d57d5314bceed08fe8267a
matheusjbk <matheusjbk@gmail.com>
2026-03-26 13:59:40 -0300

    feat: adding bcryptjs to encrypt and decrypt user's password

--- src/actions/login/loginAction.ts
+++ src/actions/login/loginAction.ts
@@ -10,6 +10,18 @@ type LoginActionState = {
 export async function loginAction(state: LoginActionState, formData: FormData) {
   await simulateDelay(3000); // manter para dificultar brute force
 
+  if (!(formData instanceof FormData))
+    return {
+      username: "",
+      error: "Dados inválidos",
+    };
+
+  const username = formData.get("username")?.toString() || "";
+  const password = formData.get("password")?.toString() || "";
+
+  const isValidUsername = username === process.env.LOGIN_USER;
+  const isValidPassword = "";
+
   return {
     username: "",
     error: "",
