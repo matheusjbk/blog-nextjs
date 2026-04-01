42b890cd272714d814b09c7c90ad4a4658442157
matheusjbk <matheusjbk@gmail.com>
2026-04-01 12:10:06 -0300

    feat: login middleware

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,5 +1,10 @@
 import { LoginForm } from "@/components/admin/LoginForm";
 import { ErrorMessage } from "@/components/ErrorMessage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login",
+};
 
 export default async function AdminLoginPage() {
   const allowLogin = Boolean(Number(process.env.ALLOW_LOGIN));
---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import { verifyjwt } from "./lib/login/manage-login";
+
+export default async function middleware(request: NextRequest) {
+  const isLoginPage = request.nextUrl.pathname.includes("login");
+  const isAdminPage = request.nextUrl.pathname.includes("/admin");
+  const isGetRequest = request.method === "GET";
+
+  const shouldBeAuthenticated = isAdminPage && !isLoginPage;
+  const shouldRedirect = shouldBeAuthenticated && isGetRequest;
+
+  if (!shouldRedirect) return NextResponse.next();
+
+  const jwt = request.cookies.get(
+    process.env.LOGIN_COOKIE_NAME || "loginSession",
+  )?.value;
+
+  const isAuthenticated = await verifyjwt(jwt);
+
+  if (!isAuthenticated) {
+    const loginUrl = new URL("/admin/login", request.url);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/admin/:path*",
+};
